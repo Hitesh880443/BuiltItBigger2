1625a27bc66874a3228383db0cc4dfa60d3ae999
Hitesh <hsutar00@gmail.com>
2016-06-23 01:14:34 +0530

    Added Product Flavours

--- app/src/free/java/com/hitesh/builditbigger/DummyFrag.java
+++ app/src/free/java/com/hitesh/builditbigger/DummyFrag.java
@@ -1,4 +1,4 @@
-package com.hitesh.builditbigger.paid;
+package com.hitesh.builditbigger;
 
 
 import android.os.Bundle;
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.hitesh.builditbigger.R;
 
 
 /**
---
+++ app/src/paid/java/com/hitesh/builditbigger/DummyFrag.java
@@ -0,0 +1,31 @@
+package com.hitesh.builditbigger;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hitesh.builditbigger.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DummyFrag extends Fragment {
+
+
+    public DummyFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dummy, container, false);
+    }
+
+}
